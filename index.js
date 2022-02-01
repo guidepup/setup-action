828085e1e30ad1aba4736b3bae9e64c2c8ec8b81
cmorten <craig.morten@hotmail.co.uk>
2022-02-01 21:40:57 +0000

    fix: ensure run in CI mode

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const process = require("process");
 const core = require("@actions/core");
 
 try {
+  process.argv.push("--ci");
   require("@guidepup/setup");
 } catch (err) {
   core.setFailed(err);
