6a68f617bcfec49b5e929d21b99ffbb2fdf50b23
cmorten <craig.morten@hotmail.co.uk>
2022-09-19 11:50:59 +0100

    fix: no top level await

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const process = require("process");
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-try {
-  await exec.exec(`"/Library/Application Support/VMware Tools/vmware-resolutionSet" 1440 900`);
-  process.argv.push("--ci");
-  require("@guidepup/setup");
-} catch (err) {
-  core.setFailed(err);
-  process.exit();
+async function main() {
+  try {
+    await exec.exec(
+      `"/Library/Application Support/VMware Tools/vmware-resolutionSet" 1440 900`
+    );
+    process.argv.push("--ci");
+    require("@guidepup/setup");
+  } catch (err) {
+    core.setFailed(err);
+    process.exit();
+  }
 }
+
+main();
