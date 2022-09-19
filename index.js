d4312ddb6f40dade49185ddba506d822a94f172a
cmorten <craig.morten@hotmail.co.uk>
2022-09-19 11:44:21 +0100

    fix: remove sudo usage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 try {
-  exec.exec(`sudo "/Library/Application Support/VMware Tools/vmware-resolutionSet" 1440 900 && sleep 5`);
+  exec.exec(`"/Library/Application Support/VMware Tools/vmware-resolutionSet" 1440 900 && sleep 5`);
   process.argv.push("--ci");
   require("@guidepup/setup");
 } catch (err) {
