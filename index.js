295e078e651ddc381e53274e2b3e1fd605583c82
cmorten <craig.morten@hotmail.co.uk>
2022-09-19 12:08:08 +0100

    feat: allow custom setting of resolution width and height

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const exec = require("@actions/exec");
 
 async function main() {
   try {
+    // Fix for GitHub actions macos-11 screencapture not working
+    // REF: https://github.com/actions/runner-images/issues/5960
+    const width = core.getInput("resolutionWidth") || "1920";
+    const height = core.getInput("resolutionHeight") || "1080";
+
     await exec.exec(
-      `"/Library/Application Support/VMware Tools/vmware-resolutionSet" 1440 900`
+      `"/Library/Application Support/VMware Tools/vmware-resolutionSet" ${width} ${height}`
     );
+
+    // Run generic screenreader setup
     process.argv.push("--ci");
     require("@guidepup/setup");
   } catch (err) {
