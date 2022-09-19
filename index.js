41c424e9a2a6898f2e2e7f65a8a9aa853edf3ca2
cmorten <craig.morten@hotmail.co.uk>
2022-09-19 11:35:28 +0100

    fix: composite syntax was incorrect

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const process = require("process");
 const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 try {
+  exec.exec(`sudo "/Library/Application Support/VMware Tools/vmware-resolutionSet" 1440 900 && sleep 5`);
   process.argv.push("--ci");
   require("@guidepup/setup");
 } catch (err) {
