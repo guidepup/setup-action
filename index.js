fde4876aa35401c25d76bb33ef2894f54f3fcfb0
cmorten <craig.morten@hotmail.co.uk>
2022-12-18 15:00:19 +0000

    fix: ensure only perform macos resolution fix on macos

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const process = require("process");
+const os = require("os");
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 async function main() {
   try {
-    // Fix for GitHub actions macos-11 screencapture not working
-    // REF: https://github.com/actions/runner-images/issues/5960
-    const width = core.getInput("resolutionWidth") || "1920";
-    const height = core.getInput("resolutionHeight") || "1080";
+    if (os.platform === "darwin") {
+      // Fix for GitHub actions macos-11 screencapture not working
+      // REF: https://github.com/actions/runner-images/issues/5960
+      const width = core.getInput("resolutionWidth") || "1920";
+      const height = core.getInput("resolutionHeight") || "1080";
 
-    await exec.exec(
-      `"/Library/Application Support/VMware Tools/vmware-resolutionSet" ${width} ${height}`
-    );
+      await exec.exec(
+        `"/Library/Application Support/VMware Tools/vmware-resolutionSet" ${width} ${height}`
+      );
+    }
 
     // Run generic screenreader setup
     process.argv.push("--ci");
