d52b5be16bee5da0988169af75d174cc8d015a4a
jlp-craigmorten <craig.morten@waitrose.co.uk>
2024-02-01 09:25:20 +0000

    tmp: log os

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const exec = require("@actions/exec");
 
 async function main() {
   try {
+    console.log(os.platform);
+
     if (os.platform === "darwin") {
       // Fix for GitHub actions macos-11 screencapture not working
       // REF: https://github.com/actions/runner-images/issues/5960
